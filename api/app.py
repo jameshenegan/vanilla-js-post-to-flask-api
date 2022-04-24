3a4d45d6b587cc65d79289eba949a552bc14bbbe
jameshenegan <jimhenegan@gmail.com>
2022-04-24 14:35:32 -0500

    first commit

---
+++ api/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__, static_folder="../frontend", static_url_path="/")
+
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
+
+@app.route("/api/fetchData", methods = ['POST'])
+def fetch_data():
+    data = request.get_json()
+    print(data)
+    return {"message": "hello"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
